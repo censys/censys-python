2877ca77f1a05d12a3ba2a79fe137a48d84444e7
Zakir Durumeric <zakird@gmail.com>
2015-10-20 16:00:49 -0400

    api validation

--- censys/ipv4.py
+++ censys/ipv4.py
@@ -42,6 +42,10 @@ class CensysIPv4Tests(unittest.TestCase):
         print json.dumps(self._api.view("84.206.102.184"))
         print "\n\n\n"
 
+    def testEmptySearch(self):
+        print json.dumps(self._api._post("search/ipv4", data={"query1":"query"}))
+        print "\n\n\n"
+
     def testSearch(self):
         print json.dumps(self._api.search("*"))
         print "\n\n\n"
