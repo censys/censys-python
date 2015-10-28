aa9102e8b34603a0974b71989382dec956de720d
Zakir Durumeric <zakird@gmail.com>
2015-10-28 16:57:07 +0900

    query definition endspoints

--- censys/query.py
+++ censys/query.py
@@ -10,6 +10,14 @@ class CensysQuery(CensysAPIBase):
         data ={"query":query}
         return self._post("query", data=data)
 
+    def get_series(self):
+        path = "/query_definitions"
+        return self._get(path)
+
+    def get_series_details(self, series):
+        path = "/".join(("query_definitions", series))
+        return self._get(path)
+
     def check_job(self, job_id):
         path = "/".join(("query", job_id))
         return self._get(path)
@@ -33,13 +41,19 @@ class CensysQueryTests(unittest.TestCase):
     def setUp(self):
         self._api = CensysQuery()
 
-    def test_query(self):
-        j = self._api.new_job(self.VALID_QUERY)
-        print json.dumps(j)
-        job_id = j["job_id"]
-        r = self._api.check_job_loop(job_id)
-        print json.dumps(r)
-        print json.dumps(self._api.get_results(job_id, 2))
+#    def test_query(self):
+#        j = self._api.new_job(self.VALID_QUERY)
+#        print json.dumps(j)
+#        job_id = j["job_id"]
+#        r = self._api.check_job_loop(job_id)
+#        print json.dumps(r)
+#        print json.dumps(self._api.get_results(job_id, 2))
+#
+    def test_get_series(self):
+        print self._api.get_series()
+
+    def test_get_series_details(self):
+        print self._api.get_series_details("ipv4")
 
 
 if __name__ == "__main__":
