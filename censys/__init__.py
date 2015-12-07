50113ace131aeebd31a1166f482dab7efed8a500
Zakir Durumeric <zakird@gmail.com>
2015-12-07 13:25:42 -0600

    new version

--- censys/__init__.py
+++ censys/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Censys Team"
 __email__ = "team@censys.io"
 __copyright__ = "Copyright 2015 Regents of the Unversity of Michigan"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __license__ = "Apache License, Version 2.0"
 
 __all__ = ["certificates", "ipv4", "websites", "query", "export"]
