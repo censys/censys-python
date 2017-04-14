39f807631cb7166171262bb118376b9a1166555d
Zakir Durumeric <zakird@gmail.com>
2017-04-14 11:25:10 -0400

    bumping to correct version

--- censys/query.py
+++ censys/query.py
@@ -38,7 +38,10 @@ class CensysQuery(CensysAPIBase):
 
 class CensysQueryTests(unittest.TestCase):
 
-    VALID_QUERY = "SELECT ip FROM ipv4.20151012 LIMIT 100"
+    VALID_QUERY = """SELECT ip FROM ipv4.20151012 where
+    p443.https.tls.certificate.parsed.extensions.key_usage.certificate_sign=true
+    order by ip desc"""
+    LONG_QUERY = "select count(*) from FROM [ipv4.20170318] i WHERE i.location.country_code = \"FI\" AND (i.p80.http.get.body CONTAINS \"content=\"wordpress 4.7.2\" or i.p80.http.get.body CONTAINS \"content=\"WordPress 4.7.2\")"
     EMPTY_QUERY = "SELECT valid_nss FROM certificates.certificates where 1=0"
     INVALID_QUERY = "SELECT nvalid FROM certificates.certificates where 1=0"
     MULTIPLE_QUERY = "SELECT ip FROM ipv4.20151012 LIMIT 100; SELECT ip FROM ipv4.20151012 LIMIT 100"
@@ -47,39 +50,43 @@ class CensysQueryTests(unittest.TestCase):
     def setUpClass(cls):
         cls._api = CensysQuery()
 
-    def test_query(self):
-        j = self._api.new_job(self.VALID_QUERY)
-        print(json.dumps(j))
+    #def test_query(self):
+    #    j = self._api.new_job(self.VALID_QUERY)
+    #    print(json.dumps(j))
+    #    job_id = j["job_id"]
+    #    r = self._api.check_job_loop(job_id)
+    #    print(json.dumps(r))
+    #    print(json.dumps(self._api.get_results(job_id, 1)))
+
+    #def test_empty_query(self):
+    #    j = self._api.new_job(self.EMPTY_QUERY)
+    #    job_id = j["job_id"]
+    #    r = self._api.check_job_loop(job_id)
+    #    print(json.dumps(r))
+    #    print(json.dumps(self._api.get_results(job_id, 1)))
+
+    #def test_invalid_query(self):
+    #    j = self._api.new_job(self.INVALID_QUERY)
+    #    job_id = j["job_id"]
+    #    r = self._api.check_job_loop(job_id)
+    #    print(json.dumps(r))
+
+    #def test_multiple_query(self):
+    #    j = self._api.new_job(self.MULTIPLE_QUERY)
+    #    job_id = j["job_id"]
+    #    r = self._api.check_job_loop(job_id)
+    #    print(json.dumps(r))
+
+    #def test_get_series(self):
+    #    print(self._api.get_series())
+
+    #def test_get_series_details(self):
+    #    print(self._api.get_series_details("ipv4"))
+
+    def test_bad_loop(self):
+        j = self._api.new_job(self.LONG_QUERY)
         job_id = j["job_id"]
-        r = self._api.check_job_loop(job_id)
-        print(json.dumps(r))
         print(json.dumps(self._api.get_results(job_id, 1)))
-
-    def test_empty_query(self):
-        j = self._api.new_job(self.EMPTY_QUERY)
-        job_id = j["job_id"]
-        r = self._api.check_job_loop(job_id)
-        print(json.dumps(r))
-        print(json.dumps(self._api.get_results(job_id, 1)))
-
-    def test_invalid_query(self):
-        j = self._api.new_job(self.INVALID_QUERY)
-        job_id = j["job_id"]
-        r = self._api.check_job_loop(job_id)
-        print(json.dumps(r))
-
-    def test_multiple_query(self):
-        j = self._api.new_job(self.MULTIPLE_QUERY)
-        job_id = j["job_id"]
-        r = self._api.check_job_loop(job_id)
-        print(json.dumps(r))
-
-
-    def test_get_series(self):
-        print(self._api.get_series())
-
-    def test_get_series_details(self):
-        print(self._api.get_series_details("ipv4"))
 
 
 if __name__ == "__main__":
