881dd3552b3450b46b2e0d11aba28f039ddf7624
Aidan Holland <aidan@censys.io>
2020-08-21 09:20:42 -0400

    chore: bump version

--- censys/__init__.py
+++ censys/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Censys Team"
 __email__ = "support@censys.io"
 __copyright__ = "Copyright 2020 Censys, Inc."
-__version__ = "0.2.0"
+__version__ = "1.0.0"
 __license__ = "Apache License, Version 2.0"
 
 __all__ = ["certificates", "ipv4", "websites"]
