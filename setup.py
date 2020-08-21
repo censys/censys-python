9f48308e5d340ab52aa5d09680bc50a0de6abcfa
Matt Clement <482829+mattclement@users.noreply.github.com>
2020-08-21 15:58:33 -0400

    Revert "Update to 1.0.0"

--- setup.py
+++ setup.py
@@ -1,62 +1,33 @@
-import io
 import os
 from setuptools import setup, find_packages
 
 import censys
 
-NAME = "censys"
-DESCRIPTION = "Python library for interacting with Censys Search Engine (censys.io)"
-URL = "https://github.com/censys/censys-python"
-
-REQUIRES_PYTHON = ">=3.6.0"
-REQUIRED = ["requests", "netaddr"]
-EXTRAS = {
-    "dev": ["flake8", "black", "pytest", "pytest-cov", "mypy"],
-}
-
 here = os.path.abspath(os.path.dirname(__file__))
 
-try:
-    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        long_description = "\n" + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
 setup(
-    name=NAME,
+    name='censys',
     version=censys.__version__,
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    description='Python library for interacting with Censys Search Engine (censys.io)',
+    long_description='Python library for interacting with Censys Search Engine (censys.io)',
+    classifiers=[
+        "Programming Language :: Python",
+    ],
     author=censys.__author__,
     author_email=censys.__email__,
     license=censys.__license__,
-    url=URL,
-    keywords=NAME,
-    python_requires=REQUIRES_PYTHON,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    url='https://github.com/censys/censys-python',
+    keywords='censys',
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    entry_points={
-        "console_scripts": [
-            # "censys = censys.__main__:main",
-            "censys-maxmind = censys.admin.maxmind:main",
-        ]
-    },
-    classifiers=[
-        "Typing :: Typed",
-        "Topic :: Internet",
-        "Topic :: Security",
-        "Framework :: Pytest",
-        "Framework :: Flake8",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "License :: OSI Approved :: Apache Software License",
+    install_requires=[
+        "requests",
+        "netaddr"
     ],
+    entry_points={
+        'console_scripts': [
+            'censys = censys.__main__:main',
+        ]
+    }
 )
