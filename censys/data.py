cd3483f41626d6f985a890a86212cc05c18bd186
Aidan Holland <aidan@censys.io>
2020-08-21 17:13:21 -0400

    chore: restore data

---
+++ censys/data.py
@@ -0,0 +1,17 @@
+from censys.base import CensysAPIBase
+
+
+class CensysData(CensysAPIBase):
+
+    _PREFIX = "/data"
+
+    def get_series(self):
+        return self._get(self._PREFIX)
+
+    def view_series(self, series: str):
+        path = "/".join([self._PREFIX, series])
+        return self._get(path)
+
+    def view_result(self, series: str, result: str):
+        path = "/".join([self._PREFIX, series, result])
+        return self._get(path)
