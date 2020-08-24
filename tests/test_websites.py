6b6601c308fd500166ca716faf3934f7f3cb9d27
Aidan Holland <thehappydinoa@gmail.com>
2020-08-24 10:36:30 -0400

    Revert "Revert "Update to 1.0.0""

---
+++ tests/test_websites.py
@@ -0,0 +1,32 @@
+import unittest
+
+from utils import required_env
+
+from censys.websites import CensysWebsites
+
+
+@required_env
+class CensysWebsitesTests(unittest.TestCase):
+
+    MAX_RECORDS = 10
+    EXCEPTED_REPORT_FIELDS = {"metadata", "results", "status"}
+
+    @classmethod
+    def setUpClass(cls):
+        cls._api = CensysWebsites()
+
+    def test_view(self):
+        response = self._api.view("google.com")
+        self.assertIsInstance(response, dict)
+
+    def test_search(self):
+        response = list(self._api.search("*", max_records=self.MAX_RECORDS))
+        self.assertLessEqual(len(response), self.MAX_RECORDS)
+
+    def test_report(self):
+        response = self._api.report("*", "80.http.get.headers.server.raw")
+        self.assertSetEqual(set(response.keys()), self.EXCEPTED_REPORT_FIELDS)
+
+
+if __name__ == "__main__":
+    unittest.main()
