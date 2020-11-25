a123cd9d46608174dae4068acfe76574a700da3e
Aidan Holland <aidan@censys.io>
2020-11-25 11:03:08 -0500

    chore: bump version

--- censys/__init__.py
+++ censys/__init__.py
@@ -5,7 +5,7 @@ An easy-to-use and lightweight API wrapper for the Censys Search Engine (censys.
 __author__ = "Censys Team"
 __email__ = "support@censys.io"
 __copyright__ = "Copyright 2020 Censys, Inc."
-__version__ = "1.0.1"
+__version__ = "1.1.0"
 __license__ = "Apache License, Version 2.0"
 
 __all__ = ["certificates", "data", "exceptions", "ipv4", "websites"]
