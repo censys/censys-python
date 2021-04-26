4551c1ef1be8b7bc8b9d7457f16fc58ddd336ea3
Aidan Holland <aidan@censys.io>
2021-04-26 09:21:36 -0400

    chore: Update __all__ for * imports

--- censys/v1/__init__.py
+++ censys/v1/__init__.py
@@ -1,6 +1,7 @@
 """Interact with the Censys Search v1 APIs."""
-from .api import CensysSearchAPIv1
 from .certificates import CensysCertificates
 from .data import CensysData
 from .ipv4 import CensysIPv4
 from .websites import CensysWebsites
+
+__all__ = ["CensysCertificates", "CensysData", "CensysIPv4", "CensysWebsites"]
--- censys/v2/__init__.py
+++ censys/v2/__init__.py
@@ -1,3 +1,4 @@
 """Interact with the Censys Search v2 APIs."""
-from .api import CensysSearchAPIv2
 from .hosts import CensysHosts
+
+__all__ = ["CensysHosts"]
