50f127c256489e74aa7dc77a726bb0d4bf2f096c
Aidan Holland <aidan@censys.io>
2021-05-03 15:12:54 -0400

    chore: update __init__

--- censys/search/__init__.py
+++ censys/search/__init__.py
@@ -1,9 +1,7 @@
 """An easy-to-use and lightweight API wrapper for Censys Search API (censys.io)."""
 from .client import SearchClient
-from ..common.exceptions import CensysException
 from .v1 import CensysCertificates, CensysData, CensysIPv4, CensysWebsites
 from .v2 import CensysHosts
-from ..common.version import __version__
 
 __copyright__ = "Copyright 2021 Censys, Inc."
 __all__ = [
@@ -13,5 +11,4 @@ __all__ = [
     "CensysIPv4",
     "CensysWebsites",
     "CensysHosts",
-    "CensysException",
 ]
