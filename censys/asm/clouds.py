d0bda510c0b00fc6cf0d7f70a75971ccfa321aeb
Aidan Holland <aidan@censys.io>
2021-05-10 10:56:03 -0400

    feat: cloud endpoints

--- censys/asm/clouds.py
+++ censys/asm/clouds.py
@@ -4,6 +4,22 @@ from typing import Union
 
 from .api import CensysAsmAPI
 
+Since = Union[str, datetime.date, datetime.datetime]
+
+
+def format_since_date(since: Since) -> str:
+    """Formats since date as ISO 8601 format.
+
+    Args:
+        since (Since): Date.
+
+    Returns:
+        str: ISO 8601 formatted date string.
+    """
+    if isinstance(since, (datetime.date, datetime.datetime)):
+        return since.strftime("%Y-%m-%d")
+    return since
+
 
 class Clouds(CensysAsmAPI):
     """Clouds API class."""
@@ -12,19 +28,61 @@ class Clouds(CensysAsmAPI):
 
     def get_host_counts(
         self,
-        since: Union[str, datetime.date, datetime.datetime],
+        since: Since,
     ) -> dict:
         """Retrieve host counts by cloud.
 
         Hosts found after the date provided in the `since` parameter will be included in the new asset counts.
 
         Args:
-            since ([str, datetime.date, datetime.datetime]): Date to include hosts from.
+            since (Since): Date to include hosts from.
 
         Returns:
             dict: Host count result.
         """
-        if isinstance(since, (datetime.date, datetime.datetime)):
-            since = since.strftime("%Y-%m-%d")
-        path = f"{self.base_path}/hostCounts/{since}"
-        return self._get(path)
+        since = format_since_date(since)
+        return self._get(f"{self.base_path}/hostCounts/{since}")
+
+    def get_domain_counts(self, since: Since) -> dict:
+        """Retrieve domain counts by cloud.
+
+        Args:
+            since (Since): Date to include domains from.
+
+        Returns:
+            dict: Domain count result.
+        """
+        since = format_since_date(since)
+        return self._get(f"{self.base_path}/domainCounts/{since}")
+
+    def get_object_store_counts(self, since: Since) -> dict:
+        """Retrieve object store counts by cloud.
+
+        Args:
+            since (Since): Date to include object stores from.
+
+        Returns:
+            dict: Object store count result.
+        """
+        since = format_since_date(since)
+        return self._get(f"{self.base_path}/objectStoreCounts/{since}")
+
+    def get_subdomain_counts(self, since: Since) -> dict:
+        """Retrieve subdomain counts by cloud.
+
+        Args:
+            since (Since): Date to include subdomains from.
+
+        Returns:
+            dict: Subdomain count result.
+        """
+        since = format_since_date(since)
+        return self._get(f"{self.base_path}/subdomainCounts/{since}")
+
+    def get_unknown_counts(self) -> dict:
+        """Retrieve known and unknown counts for hosts by cloud.
+
+        Returns:
+            dict: Unknown count result.
+        """
+        return self._get(f"{self.base_path}/unknownCounts")
