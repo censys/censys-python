a3c36eb2775bf7b9fb5035e4b1f5a689d7c1a6d8
Aidan Holland <aidan@censys.io>
2021-06-01 09:36:44 -0400

    chore: Add CLI view function

--- censys/cli/commands/__init__.py
+++ censys/cli/commands/__init__.py
@@ -2,5 +2,6 @@
 from .config import cli_asm_config, cli_config
 from .hnri import cli_hnri
 from .search import cli_search
+from .view import cli_view
 
-__all__ = ["cli_asm_config", "cli_config", "cli_hnri", "cli_search"]
+__all__ = ["cli_asm_config", "cli_config", "cli_hnri", "cli_search", "cli_view"]
---
+++ censys/cli/commands/view.py
@@ -0,0 +1,11 @@
+"""Censys view CLI."""
+import argparse
+
+
+def cli_view(args: argparse.Namespace):
+    """Search subcommand.
+
+    Args:
+        args (Namespace): Argparse Namespace.
+    """
+    pass
