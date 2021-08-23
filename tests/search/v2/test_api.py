7a1a3a28638af61e8202e133aca4793ce50d8a5d
Aidan Holland <aidan@censys.io>
2021-08-23 14:54:06 -0400

    feat: Alias account and quota

--- tests/search/v2/test_api.py
+++ tests/search/v2/test_api.py
@@ -2,10 +2,12 @@ import unittest
 from unittest.mock import mock_open, patch
 
 import pytest
+import responses
 from parameterized import parameterized
 from requests.models import Response
 
-from tests.utils import CensysTestCase
+from tests.search.v1.test_api import ACCOUNT_JSON
+from tests.utils import V1_ENDPOINT_ON_V2_URL, CensysTestCase
 
 from censys.common.exceptions import CensysException, CensysExceptionMapper
 from censys.search.v2.api import CensysSearchAPIv2
@@ -27,6 +29,19 @@ class CensysSearchAPITests(CensysTestCase):
         response.status_code = status_code
 
         assert self.api._get_exception_class(response) == exception
+
+    def test_account_and_quota(self):
+        self.responses.add(
+            responses.GET,
+            f"{V1_ENDPOINT_ON_V2_URL}/account",
+            status=200,
+            json=ACCOUNT_JSON,
+        )
+        results = self.api.account()
+        assert results == ACCOUNT_JSON
+
+        results = self.api.quota()
+        assert results == ACCOUNT_JSON["quota"]
 
 
 @patch.dict("os.environ", {"CENSYS_API_ID": "", "CENSYS_API_SECRET": ""})
