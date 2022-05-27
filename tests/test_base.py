d55e498f105dc0365182dde14cc5de2f586d7c48
Edwin Zishiri <ezishiri@censys.io>
2022-05-27 09:49:50 -0400

    test(search): Update SEARCH tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -22,25 +22,25 @@ class CensysAPIBaseTests(CensysTestCase):
     def test_base_get_exception_class(self):
         # Mock
         base = CensysAPIBase("url")
-        # Actual call/assertions 
+        # Actual call/assertions
         assert base._get_exception_class(Response()) == CensysAPIException
 
     def test_no_api_url(self):
-        # Mock 
+        # Mock
         self.mocker.patch.dict("os.environ", {"CENSYS_API_URL": ""})
-        # Actual call/error raising 
+        # Actual call/error raising
         with pytest.raises(CensysException, match="No API url configured."):
             CensysAPIBase()
 
     def test_successful_empty_json_response(self):
-        # Mock response 
+        # Mock response
         self.responses.add(
             responses.GET,
             TEST_URL + TEST_ENDPOINT,
             status=200,
             body=None,
         )
-        # Actual call 
+        # Actual call
         base = CensysAPIBase(TEST_URL)
         # Assertions
         assert base._get(TEST_ENDPOINT) == {
@@ -49,37 +49,37 @@ class CensysAPIBaseTests(CensysTestCase):
         }
 
     def test_successful_error_json_response(self):
-        # Mock response 
+        # Mock response
         self.responses.add(
             responses.GET,
             TEST_URL + TEST_ENDPOINT,
             status=200,
             json=ERROR_JSON,
         )
-        # Actual call 
+        # Actual call
         base = CensysAPIBase(TEST_URL)
-        # Assertion/error raising 
+        # Assertion/error raising
         with pytest.raises(CensysAPIException, match=ERROR_JSON["error"]):
             base._get(TEST_ENDPOINT)
 
     def test_invalid_json_response(self):
-        # Mock response 
+        # Mock response
         self.responses.add(
             responses.GET,
             TEST_URL + TEST_ENDPOINT,
             status=400,
             body="<html><h1>Definitely not JSON</h1>",
         )
-        # Actual call 
+        # Actual call
         base = CensysAPIBase(TEST_URL)
-        # Assertion/error raising 
+        # Assertion/error raising
         with pytest.raises(
             CensysAPIException, match="is not valid JSON and cannot be decoded"
         ):
             base._get(TEST_ENDPOINT)
 
     def test_default_user_agent(self):
-        # Mock/actual call  
+        # Mock/actual call
         base = CensysAPIBase(TEST_URL)
         # Assertions
         assert (
@@ -88,9 +88,9 @@ class CensysAPIBaseTests(CensysTestCase):
         )
 
     def test_user_agent(self):
-        # Mock/actual call 
+        # Mock/actual call
         base = CensysAPIBase(TEST_URL, user_agent="test")
-        # Assertions 
+        # Assertions
         assert (
             base._session.headers["User-Agent"]
             == requests.utils.default_user_agent() + " test"
@@ -98,13 +98,13 @@ class CensysAPIBaseTests(CensysTestCase):
 
     @pytest.mark.filterwarnings("ignore:HTTP proxies will not be used.")
     def test_proxies(self):
-        # Mock/actual call 
+        # Mock/actual call
         base = CensysAPIBase(TEST_URL, proxies={"http": "test", "https": "tests"})
-        # Assertions 
+        # Assertions
         assert list(base._session.proxies.keys()) == ["https"]
 
     def test_cookies(self):
-        # Mock/actual call 
+        # Mock/actual call
         base = CensysAPIBase(TEST_URL, cookies={"_ga": "GA"})
-        # Assertions 
+        # Assertions
         assert list(base._session.cookies.keys()) == ["_ga"]
