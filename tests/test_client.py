2a0b71f61b3ad969511f82a466e661f35e87129b
Edwin Zishiri <40304716+ezishiri@users.noreply.github.com>
2022-05-27 12:01:05 -0400

    test(search): Update Search tests  (#311)
    * test(search): Update search tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -13,15 +13,19 @@ class CensysSearchClientTest(CensysTestCase):
         self.expected_auth = (self.api_id, self.api_secret)
 
     def test_api_creds_args(self):
+        # Actual call
         client = SearchClient(self.api_id, self.api_secret)
         for version, indexes in ALL_INDEXES.items():
             v = getattr(client, version)
             for index in indexes:
+                # Assertions
                 assert getattr(v, index)._session.auth == self.expected_auth
 
     def test_api_creds_kwargs(self):
+        # Actual call
         client = SearchClient(api_id=self.api_id, api_secret=self.api_secret)
         for version, indexes in ALL_INDEXES.items():
             v = getattr(client, version)
             for index in indexes:
+                # Assertions
                 assert getattr(v, index)._session.auth == self.expected_auth
