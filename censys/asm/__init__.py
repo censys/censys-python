4a7a1779510c1e3b3a5b3992bbcfec3563b2ddaa
Aidan Holland <aidan@censys.io>
2022-10-01 10:43:05 -0400

    feat(asm-api): Add Inventory Search

--- censys/asm/__init__.py
+++ censys/asm/__init__.py
@@ -3,6 +3,7 @@ from .assets import Assets, CertificatesAssets, DomainsAssets, HostsAssets
 from .client import AsmClient
 from .clouds import Clouds
 from .events import Events
+from .inventory import InventorySearch
 from .risks import Risks, Risksv1, Risksv2
 from .seeds import Seeds
 
@@ -14,6 +15,7 @@ __all__ = [
     "Risksv1",
     "Risksv2",
     "Seeds",
+    "InventorySearch",
     "Assets",
     "CertificatesAssets",
     "DomainsAssets",
