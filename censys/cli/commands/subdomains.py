057a1dda693c0b6be0d15ef1e8fbc4b498ef101b
Aidan Holland <aidan@censys.io>
2023-04-25 14:58:53 -0400

    chore(cli): update subdomains command

--- censys/cli/commands/subdomains.py
+++ censys/cli/commands/subdomains.py
@@ -10,7 +10,7 @@ from censys.common.exceptions import (
     CensysRateLimitExceededException,
     CensysUnauthorizedException,
 )
-from censys.search import CensysCertificates
+from censys.search import CensysCerts
 
 
 def print_subdomains(subdomains: Set[str], as_json: bool = False):
@@ -35,24 +35,23 @@ def cli_subdomains(args: argparse.Namespace):  # pragma: no cover
     """
     subdomains = set()
     try:
-        client = CensysCertificates(api_id=args.api_id, api_secret=args.api_secret)
-        certificate_query = f"parsed.names: {args.domain}"
+        client = CensysCerts(api_id=args.api_id, api_secret=args.api_secret)
+        certificate_query = f"names: {args.domain}"
 
         with err_console.status(f"Querying {args.domain} subdomains"):
-            certificates_search_results = client.search(
-                certificate_query, fields=["parsed.names"], max_records=args.max_records
-            )
+            query = client.search(certificate_query, fields=["names"], pages=args.pages)
 
             # Flatten the result, and remove duplicates
-            for search_result in certificates_search_results:
-                new_subdomains: List[str] = search_result.get("parsed.names", [])
-                subdomains.update(
-                    [
-                        subdomain
-                        for subdomain in new_subdomains
-                        if subdomain.endswith(args.domain)
-                    ]
-                )
+            for hits in query:
+                for cert in hits:
+                    new_subdomains: List[str] = cert.get("names", [])
+                    subdomains.update(
+                        [
+                            subdomain
+                            for subdomain in new_subdomains
+                            if subdomain.endswith(args.domain)
+                        ]
+                    )
 
         # Don't make console prints if we're in json mode
         if not args.json:
@@ -71,6 +70,11 @@ def cli_subdomains(args: argparse.Namespace):  # pragma: no cover
         sys.exit(1)
     except CensysException as e:
         err_console.print(str(e))
+        print_subdomains(subdomains, args.json)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        err_console.print("Caught Ctrl-C, exiting...")
+        print_subdomains(subdomains, args.json)
         sys.exit(1)
 
 
@@ -89,7 +93,7 @@ def include(parent_parser: argparse._SubParsersAction, parents: dict):
     )
     subdomains_parser.add_argument("domain", help="The base domain to search for")
     subdomains_parser.add_argument(
-        "--max-records", type=int, default=100, help="Max records to query"
+        "--pages", type=int, default=1, help="Max records to query"
     )
     subdomains_parser.add_argument(
         "-j", "--json", action="store_true", help="Output in JSON format"
